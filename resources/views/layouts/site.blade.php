ca66dcc04d9aed6ca5ec932255f5bd67d5ef0b9b
viktorbeznosov <you@example.com>
2020-07-25 20:42:30 +0300

    *

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -65,9 +65,9 @@
                     <h2 class="ftco-heading-2">About Us</h2>
                     <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li class="ftco-animate"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li class="ftco-animate"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                     </ul>
                 </div>
             </div>
@@ -79,9 +79,9 @@
                         <div class="text">
                             <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
                             <div class="meta">
-                                <div><a href="#"><span class="icon-calendar"></span> Sept 15, 2018</a></div>
-                                <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                                <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                                <div><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> Sept 15, 2018</a></div>
+                                <div><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Admin</a></div>
+                                <div><a href="#"><i class="fa fa-comment" aria-hidden="true"></i> 19</a></div>
                             </div>
                         </div>
                     </div>
@@ -90,9 +90,9 @@
                         <div class="text">
                             <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
                             <div class="meta">
-                                <div><a href="#"><span class="icon-calendar"></span> Sept 15, 2018</a></div>
-                                <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                                <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                                <div><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> Sept 15, 2018</a></div>
+                                <div><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Admin</a></div>
+                                <div><a href="#"><i class="fa fa-comment" aria-hidden="true"></i> 19</a></div>
                             </div>
                         </div>
                     </div>
@@ -114,9 +114,9 @@
                     <h2 class="ftco-heading-2">Have a Questions?</h2>
                     <div class="block-23 mb-3">
                         <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                            <li><i class="fa fa-map-marker" aria-hidden="true"></i><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                            <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i></span><span class="text">+2 392 3929 210</span></a></li>
+                            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i><span class="text">info@yourdomain.com</span></a></li>
                         </ul>
                     </div>
                 </div>

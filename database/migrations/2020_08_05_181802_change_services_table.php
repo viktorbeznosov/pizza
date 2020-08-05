304151b9aa5b5d2ed68ab2422f767dfceaddba20
viktorbeznosov <you@example.com>
2020-08-05 21:25:12 +0300

    add description column in services table

---
+++ database/migrations/2020_08_05_181802_change_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->text('description')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            //
+        });
+    }
+}

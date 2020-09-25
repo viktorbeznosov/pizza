0a848a1fa919cd8ac0f2f4e9a78b96a16ff7cae1
viktorbeznosov <viktorbeznosov@mail.ru>
2020-09-25 14:22:45 +0300

    add Account Cabinet

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class AccountController extends Controller
 {
@@ -15,5 +17,31 @@ class AccountController extends Controller
             'user' => $user
         );  
         return view('account', $data);
+    }
+    
+    public function update(Request $request){
+        if ($request->get('password') && $request->get('confirm_pass')){
+            $input = $request->except('_token');   
+        } else {
+            $input = $request->except('_token','password','confirm_pass');              
+        }
+
+        $messages = array(
+            'required' => 'Поле :attribute обязательно к заполнению',
+            'unique' => 'Поле :attribute должно быть уникальным',
+            'same' => 'Введенные пароли должны совпадать'
+        );
+        $validator = Validator::make($input, array(
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users,email,'.Auth::user()->id,
+            'phone' => 'required',
+            'confirm_pass' => 'same:password'
+        ),$messages);
+        if($validator->fails()){
+            dd($validator->errors());
+            return redirect()->route('account')->withErrors($validator)->withInput();
+        }
+            
+        dd($input);
     }
 }
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.site')
 
 @section('content')
+    <section class="ftco-section ftco-degree-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 sidebar ftco-animate">
+                    <div class="product d-flex">
+                        <div class="bio align-self-md-center mr-5">
+                            <img src="{{ asset($user->image) }}" alt="Image placeholder" class="img-fluid mb-4">
+                        </div>
+                    </div>
+                </div>                
+                <div class="col-md-8 ftco-animate">
 
+                    <form action="{{ route('account_update') }}" class="contact-form" method="POST">
+                        {{ csrf_field() }}
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input name="name" type="text" class="form-control" value="{{ $user->name }}" placeholder="Your Name">
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input name="email" type="text" class="form-control" value="{{ $user->email }}" placeholder="Your Email">
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input name="phone" type="text" class="form-control" value="{{ $user->phone }}" placeholder="Your Phone">
+                                </div>
+                            </div>                            
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input name="password" type="password" class="form-control" value="" placeholder="Your Password">
+                                </div>
+                            </div>  
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input name="confirm_pass" type="password" class="form-control" value="" placeholder="Confirm Password">
+                                </div>
+                            </div>                              
+                        </div>
+          
+                        <div class="form-group">
+                            <input type="submit" value="Submit" class="btn btn-primary py-3 px-5">
+                        </div>
+                    </form>
+  
+                </div> <!-- .col-md-8 -->
+            </div>
+        </div>
+    </section> <!-- .section -->
 @endsection

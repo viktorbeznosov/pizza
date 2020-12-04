8e2284b25185f9853c5416f56b69b5c8393105ab
viktorbeznosov <viktorbeznosov@mail.ru>
2020-12-04 12:57:44 +0300

    change notifications migration

--- database/migrations/2020_12_04_094130_create_notifications_table.php
+++ database/migrations/2020_12_04_094130_create_notifications_table.php
@@ -19,6 +19,7 @@ class CreateNotificationsTable extends Migration
             $table->string('type');
             $table->string('message');
             $table->text('info');
+            $table->tinyInteger('read')->default(0);
         });
     }
 

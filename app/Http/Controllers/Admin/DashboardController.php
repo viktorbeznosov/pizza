04b00d95ef6aae627c69e23cbb88da92f6b4ff01
viktorbeznosov <viktorbeznosov@mail.ru>
2020-12-24 11:04:33 +0300

    dashboard users

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,25 +4,34 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class DashboardController extends Controller
 {
     public function getUsers(Request $request){
+        
+        $intervals = array();
+        
+        for($i = 0; $i < 12; $i++){
+            $dateYearAgoMin = new \DateTime;
+            $dateYearAgoMax = new \DateTime;
+            $dateYearAgoMin->modify('-1 year');
+            $dateYearAgoMax->modify('-1 year');
+            
+            $dateYearAgoMin->modify('+ ' . ($i) . ' month');
+            $dateYearAgoMax->modify('+ ' . ($i+1) . ' month');
+       
+            $users = User::wherebetween('created_at', [$dateYearAgoMin,$dateYearAgoMax])->get();
+            
+            $intervals[] = array(
+                $dateYearAgoMax->format('m') . '.' . $dateYearAgoMax->format('Y'),
+                count($users)
+            );
+            
+        }
+        
         print_r(
-            json_encode(array(
-                ['01.2020', 5],
-                ['02.2020', 10],
-                ['03.2020', 20],
-                ['04.2020', 25],
-                ['05.2020', 30],
-                ['06.2020', 20],
-                ['07.2020', 25],
-                ['08.2020', 50],
-                ['09.2020', 30],
-                ['10.2020', 20],
-                ['11.2020', 10],
-                ['12.2020', 10],
-            ), JSON_UNESCAPED_UNICODE)
+            json_encode($intervals, JSON_UNESCAPED_UNICODE)
         );
     }
 }
